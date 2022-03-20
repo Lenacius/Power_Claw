216eac0e0fec351f6582a6d4dd72cf1330d84047
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2022-03-20 15:37:40 -0300

    Analogic Input Process Added
    Added a logic for processing the analog input for the pin A0 to A4 that are controled by 10k Ohms potentiometer powered by 5V.

--- Arduino_PowerClaw/Arduino_PowerClaw.ino
+++ Arduino_PowerClaw/Arduino_PowerClaw.ino
@@ -3,11 +3,18 @@
  */
 
 void setup() {
-  // put your setup code here, to run once:
-
+// Analog pin doesn't need to be initialized;
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float analogPin[5] = {0, 0, 0, 0, 0};
 
+  for(int x = 0; x < 5; x++){
+    convert_analog_input(&analogPin[x], x);
+  }
+}
+
+void convert_analog_input(float *output, int pin){
+  *output = (analogRead(pin) / 1023.0) * 100.0;
 }
