ddc61d439ff98a4b261e95eedf6249e922e5d8a0
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2022-03-31 19:46:09 -0300

    Setup for MPU6050 sensor readings Added
    Added the libraries needed and the setup configuration for the use of MPU6050 sensor.

--- Arduino_PowerClaw/Arduino_PowerClaw.ino
+++ Arduino_PowerClaw/Arduino_PowerClaw.ino
@@ -2,9 +2,14 @@
  * Projeto de Plataforma Alternativa - Power Claw
  */
 
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
 void setup() {
-// Analog pin doesn't need to be initialized;
-  Serial.begin(9600);
+  SetupMPU6050();
 }
 
 void loop() {
@@ -23,4 +28,32 @@ void loop() {
 
 void convert_analog_input(float *output, int pin){
   *output = (analogRead(pin) / 1023.0) * 100.0;
+}
+
+void SetupMPU6050(){
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  // set accelerometer range to +-8G
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+  // set gyro range to +- 500 deg/s
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+  // set filter bandwidth to 21 Hz
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  delay(100);
 }
