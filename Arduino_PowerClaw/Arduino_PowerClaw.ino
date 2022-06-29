9e207d4e189aeaa5b8f6a7d75443f954cf7c84a6
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2022-06-29 19:53:30 -0300

    Final stable version reading 2 MPUs

--- Arduino_PowerClaw/Arduino_PowerClaw.ino
+++ Arduino_PowerClaw/Arduino_PowerClaw.ino
@@ -1,13 +1,7 @@
 /*
  * Projeto de Plataforma Alternativa - Power Claw
  */
-
-// MPU6050 Sensor libries from Adafruit
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
 #include <Wire.h>
-
-Adafruit_MPU6050 mpu;
 
 // Finger bend values
 float analogPin[5] = {0, 0, 0, 0, 0};
@@ -15,13 +9,25 @@ float analogPin[5] = {0, 0, 0, 0, 0};
 // Serial Output
 String serialOutput;
 
+const int  MPU1 = 0x68, // AD0 Pin disconnected/grounded
+           MPU2 = 0x69; // AD0 Pin connected to 3v3
+
+float aX, aY, aZ; // Accelerometer values
+float temp;       // Temperature value    
+float gX, gY, gZ; // Gyroscope values
+
 void setup() {
-  SetupMPU6050();
+  Serial.begin(9600);
+  
+  InitMPU(MPU1);
+  InitMPU(MPU2);
 }
 
 void loop() {
   ReadAnalogPins();
-  MPU6050Read();
+  
+  GetMPU(MPU1);
+  GetMPU(MPU2);
 }
 
 void ReadAnalogPins(){
@@ -29,63 +35,65 @@ void ReadAnalogPins(){
     convert_analog_input(&analogPin[x], x);
     if(x == 0)
       serialOutput = "FNG|" + (String)analogPin[0];
+//      serialOutput = "FNG|0";
     else
       serialOutput += '|' + (String)analogPin[x];
+//      serialOutput += "|0";
   }
   Serial.println(serialOutput);
 }
 
 void convert_analog_input(float *output, int pin){
-  *output = (analogRead(pin) / 1023.0f) * 1000.0f;
+  *output = (analogRead(pin) / 1023.0f) * 10.0f;
 }
 
-void SetupMPU6050(){
-  Serial.begin(115200);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
-
-  Serial.println("Adafruit MPU6050 test!");
-
-  // Try to initialize!
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
-  Serial.println("MPU6050 Found!");
-
-  // set accelerometer range to +-8G
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-
-  // set gyro range to +- 500 deg/s
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-
-  // set filter bandwidth to 21 Hz
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-
-  delay(100);
+void InitMPU(const int MPU){
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0); 
+  Wire.endTransmission(true);
+  
+  delay(20);
 }
 
-void MPU6050Read(){  
-  /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+void GetMPU(const int MPU){
+  Wire.beginTransmission(MPU); 
+  Wire.write(0x3B);
+  Wire.endTransmission();
+  Wire.requestFrom(MPU,14,true);
+  
+  while(Wire.available() < 6);
+  
+  aX   = (Wire.read()<<8|Wire.read()) / 16384.0;  //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  aY   = (Wire.read()<<8|Wire.read()) / 16384.0;  //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  aZ   = (Wire.read()<<8|Wire.read()) / 16384.0;  //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  temp = (Wire.read()<<8|Wire.read());  //0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  gX   = (Wire.read()<<8|Wire.read()) / 131.0;  //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  gY   = (Wire.read()<<8|Wire.read()) / 131.0;  //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  gZ   = (Wire.read()<<8|Wire.read()) / 131.0;  //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  
+  if(MPU == MPU1)
+    serialOutput = "MPU1";
+  else if (MPU == MPU2)
+    serialOutput = "MPU2";
 
-  serialOutput = "MPU";
+  // Envia os valores do acelerometro para a serial (X, Y, Z)
+  serialOutput += '|' + (String)(aX);
+  serialOutput += '|' + (String)(aY);
+  serialOutput += '|' + (String)(aZ);
+  
+  //Envia valor da temperatura para a serial
+  //Calcula a temperatura em graus Celsius
+  //Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53 * 10));
 
-  // Accelerometer uses m/s^2
-  //serialOutput += '|' + (String)a.acceleration.x;
-  //serialOutput += '|' + (String)a.acceleration.y;
-  //serialOutput += '|' + (String)a.acceleration.z;
-
-  // Gyroscope uses rad/s | Sending formatted to UNITY
-  serialOutput += '|' + (String)g.gyro.x;
-  serialOutput += '|' + (String)(g.gyro.z * -1.0f);
-  serialOutput += '|' + (String)g.gyro.y;
-
-  // Temperature uses degC
-  //serialOutput += '|' + (String)temp.temperature;
+  // Envia os valores do giroscopio para a serial (X, Y, Z)
+  serialOutput += '|' + (String)(gX);
+  serialOutput += '|' + (String)(-gZ);
+  serialOutput += '|' + (String)(gY);
 
   Serial.println(serialOutput);
+
+  while(Serial.available()) // garante a limpeza do buffer
+  Serial.read();
 }
